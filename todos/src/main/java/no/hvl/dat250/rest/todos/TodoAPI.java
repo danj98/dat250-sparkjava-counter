1c9cb95ae72ef3fe217dda7a913d7b153667c776
Dan Joar Heimvik <danjoar@gmail.com>
2022-09-30 11:06:51 +0200

    added http-methods

--- todos/src/main/java/no/hvl/dat250/rest/todos/TodoAPI.java
+++ todos/src/main/java/no/hvl/dat250/rest/todos/TodoAPI.java
@@ -1,11 +1,20 @@
 package no.hvl.dat250.rest.todos;
 
-import static spark.Spark.*;
+import com.google.gson.Gson;
+import spark.Spark;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static spark.Spark.*;
 /**
  * Rest-Endpoint.
  */
 public class TodoAPI {
+
+    private static final HashMap<Long, Todo> todos = new HashMap<>();
+    private static final Gson gson = new Gson();
 
     public static void main(String[] args) {
         if (args.length > 0) {
@@ -15,7 +24,29 @@ public class TodoAPI {
         }
 
         after((req, res) -> res.type("application/json"));
-
         // TODO: Implement API, such that the testcases succeed.
+        // Get all
+        get("/todos", (req, res) -> gson.toJson(todos));
+        // Get one
+        get("todos/:id", (req, res) -> {
+            Long id = Long.valueOf(req.params(":id"));
+            return gson.toJson(todos.get(id));
+        });
+
+        post("/todos", (req, res) -> {
+            Todo todo = gson.fromJson(req.body(), Todo.class);
+            if (todo.getId() == null) {
+                todo = new Todo((long) todos.size()+1, todo.getSummary(), todo.getDescription());
+            }
+            todos.put(todo.getId(), todo);
+            return gson.toJson(todo);
+        });
+
+        put("/todos/:id", (req, res) -> {
+            Long id = Long.valueOf(req.params(":id"));
+            todos.put(id, gson.fromJson(req.body(), Todo.class));
+            return gson.toJson(todos.get(id));
+        });
+
     }
 }
